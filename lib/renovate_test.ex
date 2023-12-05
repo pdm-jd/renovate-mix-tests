0e1249a454fcb5c78767f9923d1c60bb25e95e98
Josiah Dahl <josiah.dahl@podium.com>
2023-12-05 12:05:17 -0800

    initial commit

---
+++ lib/renovate_test.ex
@@ -0,0 +1,18 @@
+defmodule RenovateTest do
+  @moduledoc """
+  Documentation for `RenovateTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RenovateTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/renovate_test_test.exs
@@ -0,0 +1,8 @@
+defmodule RenovateTestTest do
+  use ExUnit.Case
+  doctest RenovateTest
+
+  test "greets the world" do
+    assert RenovateTest.hello() == :world
+  end
+end
