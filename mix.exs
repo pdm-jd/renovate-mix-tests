f311e962854cb0ad5bbe1ceb0232d94ecefbfe48
Josiah Dahl <josiah.dahl@podium.com>
2023-12-05 23:05:56 -0800

    wip

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule RenovateTest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tesla, "1.5.0"},
+      {:tesla, "~> 1.4"},
       {:poison, "~> 4.0"}
     ]
   end
